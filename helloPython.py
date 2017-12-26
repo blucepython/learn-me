ca4ad1eaa482439b0cd79f8a2aeaa0014dfd2293
blucepython <357790753@qq.com>
2017-12-26 23:36:48 +0800

    1

--- helloPython.py
+++ helloPython.py
@@ -1,7 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 def hello();
    print 'hello world';
    return;
+=======
+print ¡®hello world¡¯£»
+>>>>>>> parent of 5a0662b... change to English
 =======
 print ¡®hello world¡¯£»
 >>>>>>> parent of 5a0662b... change to English
