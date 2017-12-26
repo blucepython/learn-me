855c87a8704f114e9e16d596105a77f357a1b676
blucepython <357790753@qq.com>
2017-12-26 23:39:39 +0800

    Revert "1"
    This reverts commit c110bd448b929011a35aaa1c24c5795274a23d81.

--- helloPython.py
+++ helloPython.py
@@ -1,13 +1,9 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 def hello();
    print 'hello world';
    return;
-=======
-print ¡®hello world¡¯£»
->>>>>>> parent of 5a0662b... change to English
 =======
 print ¡®hello world¡¯£»
 >>>>>>> parent of 5a0662b... change to English
