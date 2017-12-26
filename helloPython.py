cd935fd520cb8a847ebdcdf4f80e15bf00eac167
blucepython <357790753@qq.com>
2017-12-26 23:37:29 +0800

    Revert "change to English"
    This reverts commit 5a0662b22de4966703caa44271e803419bdad890.

--- helloPython.py
+++ helloPython.py
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 def hello();
    print 'hello world';
    return;
+=======
+print ¡®hello world¡¯£»
+>>>>>>> parent of 5a0662b... change to English
 =======
 print ¡®hello world¡¯£»
 >>>>>>> parent of 5a0662b... change to English
